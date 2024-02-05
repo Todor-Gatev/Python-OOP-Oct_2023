2d14399fcdc70f99d867080cb90ccb5cae61abce
Todor-Gatev <aleko_stastlivetsa@yahoo.com>
2024-02-05 08:56:46 +0200

    New and Updates

---
+++ 04_OOP/Exams-OOP/Exams/17-/22_October_2023/test_22_10_2023.py
@@ -0,0 +1,12 @@
+# Change to python code Judge
+# d = {x: 0 for x in [1, 2, 3]}
+#
+# print(d)
+# l = [1, 2, 3]
+# print(l[1])
+# print(l[7])
+
+with open("file.txt", "rb") as f:
+    f.read()
+    # f.write("Hello World!!!")
+
